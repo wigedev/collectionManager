be0c43fa157381272954f841c6faa33349c36146
William Eldridge <wige@comcast.net>
2020-01-10 00:35:13 -0500

    Another restructure of index.php

--- public/index.php
+++ public/index.php
@@ -13,17 +13,18 @@ use function WigeDev\JasperCore\FW;
  * public folder.
  */
 define('_ROOT_PATH_', dirname(__DIR__));
-
 /**
  * Load the composer autoloader
  */
 require_once(_ROOT_PATH_ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
-
+/**
+ * Set up any features such as logging or dependency injection, and add them to the framework here.
+ */
 /**
  * Initialize the framework.
  */
 Core::_init();
 /**
- * Set up any features such as logging or dependency injection, and add them to the framework here.
+ * Execute the request
  */
 FW()->run();
